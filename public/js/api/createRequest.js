3bbfaa177cc2f8282dc06ae65ceae5aa2a525757
Vyacheslav <slavik.kurganov.26@mail.ru>
2026-05-15 17:49:25 +0300

    В файле createRequest.js реализовал функцию для совершения запросов

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,53 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    const {
+        url,
+        method = 'GET',
+        data,
+        callback,
+    } = options;
 
+    const xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+
+    if (method.toUpperCase() === 'GET') {
+        if (data) {
+            const dataArr = Object.entries(data);
+            const dataString = dataArr.map(item => {
+                return `${encodeURIComponent(item[0])}=${encodeURIComponent(item[1])}`
+            }).join('&');
+
+            const finalUrl = `${url}?${dataString}`;
+            xhr.open(method, finalUrl);
+            xhr.send();
+        }else  {
+            xhr.open(method, url);
+            xhr.send();
+        }
+
+    }else {
+        const formData = new FormData();
+            for (const [key, value] of Object.entries(data)) {
+                formData.append(key, value)
+            }
+
+            xhr.open(method, url);
+            xhr.send(formData);
+    }
+
+    xhr.onload = () => {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            callback(null, xhr.response);
+        } else {
+          callback(new Error(`Ошибка: ${xhr.statusText}`));
+        }
+    };
+
+    xhr.onerror = () => {
+        callback(new Error('Ошибка сети'));
+    };
 };
+
+
