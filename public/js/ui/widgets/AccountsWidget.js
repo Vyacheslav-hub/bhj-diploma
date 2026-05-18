d362e4c9f744593b807a17d8193cdadca58e82b0
Vyacheslav <slavik.kurganov.26@mail.ru>
2026-05-18 18:10:03 +0300

    В файле AccountsWidget.js реализовал методы

--- public/js/ui/widgets/AccountsWidget.js
+++ public/js/ui/widgets/AccountsWidget.js
@@ -14,7 +14,10 @@ class AccountsWidget {
    * необходимо выкинуть ошибку.
    * */
   constructor( element ) {
-
+    if (!element) throw new Error('Не передан элемент')
+    this.element = element;
+    this.registerEvents();
+    this.update();
   }
 
   /**
@@ -25,7 +28,17 @@ class AccountsWidget {
    * вызывает AccountsWidget.onSelectAccount()
    * */
   registerEvents() {
+    this.element.addEventListener('click', (e) => {
+      if (e.target.closest('.create-account')) {
+        App.getModal('createAccount').open();
+        return;
+      }
 
+      const account = e.target.closest('.account')
+      if (account) {
+        this.onSelectAccount(account);
+      }
+    })
   }
 
   /**
@@ -39,7 +52,14 @@ class AccountsWidget {
    * метода renderItems()
    * */
   update() {
-
+  if (User.current()) {
+    Account.list((err, response) => {
+      if (response && response.success) {
+        this.clear();
+        this.renderItems(response.data)
+      }
+    })
+  }
   }
 
   /**
@@ -48,7 +68,7 @@ class AccountsWidget {
    * в боковой колонке
    * */
   clear() {
-
+    this.element.querySelectorAll('.account').forEach(el => el.remove());
   }
 
   /**
@@ -59,7 +79,13 @@ class AccountsWidget {
    * Вызывает App.showPage( 'transactions', { account_id: id_счёта });
    * */
   onSelectAccount( element ) {
-
+    const accountActive = this.element.querySelector('.account.active');
+    if (accountActive) {
+      accountActive.classList.remove('active');
+    }
+    element.classList.add('active');
+    const id = element.dataset.id;
+    App.showPage('transactions', { account_id: id });
   }
 
   /**
@@ -68,7 +94,17 @@ class AccountsWidget {
    * item - объект с данными о счёте
    * */
   getAccountHTML(item){
+    const {
+      name,
+      sum,
+      id
+    } = item;
+    const sumString = Number(sum).toLocaleString('en-US', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    });
 
+    return `<li class="account" data-id="${id}"><a href="#"><span>${name}</span><span>${sumString}</span></a></li>`
   }
 
   /**
@@ -78,6 +114,12 @@ class AccountsWidget {
    * и добавляет его внутрь элемента виджета
    * */
   renderItems(data){
+    let html = '';
 
+    data.forEach(item => {
+      html += this.getAccountHTML(item);
+    });
+
+    this.element.insertAdjacentHTML('beforeend', html);
   }
 }
