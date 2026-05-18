56f64fa0c05b82707c29aaa7abca6ec833735a8e
Vyacheslav <slavik.kurganov.26@mail.ru>
2026-05-18 14:53:52 +0300

    В файле AsyncForm.js реализовал все нужные методы

--- public/js/ui/forms/AsyncForm.js
+++ public/js/ui/forms/AsyncForm.js
@@ -13,7 +13,9 @@ class AsyncForm {
    * через registerEvents()
    * */
   constructor(element) {
-
+    if (!element) throw new Error('Не передан элемент')
+    this.element = element;
+    this.registerEvents();
   }
 
   /**
@@ -21,7 +23,10 @@ class AsyncForm {
    * вызывает метод submit()
    * */
   registerEvents() {
-
+    this.element.addEventListener('submit', (e) => {
+        e.preventDefault();
+        this.submit();
+      });
   }
 
   /**
@@ -32,7 +37,15 @@ class AsyncForm {
    * }
    * */
   getData() {
+    const formData = new FormData(this.element);
+    const entries = formData.entries();
+    const data = {};
 
+    for (let item of entries) {
+      const key = item[ 0 ];
+      data[key] = item[1];
+    }
+    return data;
   }
 
   onSubmit(options){
@@ -44,6 +57,7 @@ class AsyncForm {
    * данные, полученные из метода getData()
    * */
   submit() {
-
+    const data = this.getData();
+    this.onSubmit(data);
   }
 }
