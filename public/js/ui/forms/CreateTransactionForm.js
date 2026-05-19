9f9d894c48be4b4866cb77f01a4cfeb9232bdf21
Vyacheslav <slavik.kurganov.26@mail.ru>
2026-05-19 14:13:15 +0300

    В файле CreateTransactionForm.js реализовал все нужные методы

--- public/js/ui/forms/CreateTransactionForm.js
+++ public/js/ui/forms/CreateTransactionForm.js
@@ -9,6 +9,7 @@ class CreateTransactionForm extends AsyncForm {
    * */
   constructor(element) {
     super(element)
+    this.renderAccountsList();
   }
 
   /**
@@ -16,7 +17,18 @@ class CreateTransactionForm extends AsyncForm {
    * Обновляет в форме всплывающего окна выпадающий список
    * */
   renderAccountsList() {
+    Account.list({}, (err, response) => {
+      if (response && response.success) {
+        const select = this.element.querySelector('.accounts-select');
 
+        let option = ''
+        response.data.forEach(({id, name}) => {
+          option += `<option value="${id}">${name}</option>`;
+        });
+
+        select.innerHTML = option;
+      }
+    })
   }
 
   /**
@@ -26,6 +38,18 @@ class CreateTransactionForm extends AsyncForm {
    * в котором находится форма
    * */
   onSubmit(data) {
+    Transaction.create(data, (err, response) => {
+      if (response && response.success) {
+        this.element.reset();
 
+        App.update();
+
+        if (this.element.id === 'new-income-form') {
+          App.getModal('newIncome').close();
+        }else {
+          App.getModal('newExpense').close();
+        }
+      }
+    })
   }
 }
